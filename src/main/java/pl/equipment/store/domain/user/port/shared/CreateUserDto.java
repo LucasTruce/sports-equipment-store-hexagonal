1503ab998a9b443030eb3632364deb782fcdea38
Adrian <adrian.jaczynski@gmail.com>
2021-01-25 23:57:54 +0100

    delete @Setter

--- src/main/java/pl/equipment/store/domain/user/port/shared/CreateUserDto.java
+++ src/main/java/pl/equipment/store/domain/user/port/shared/CreateUserDto.java
@@ -2,10 +2,8 @@ package pl.equipment.store.domain.user.port.shared;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @RequiredArgsConstructor
 public class CreateUserDto {
     private final String username;
--- src/main/java/pl/equipment/store/domain/user/port/shared/UserDto.java
+++ src/main/java/pl/equipment/store/domain/user/port/shared/UserDto.java
@@ -2,12 +2,10 @@ package pl.equipment.store.domain.user.port.shared;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @RequiredArgsConstructor
 public class UserDto {
     private final Long id;
